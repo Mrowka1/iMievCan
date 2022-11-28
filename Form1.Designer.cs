b27c3e51d988575f9e6c78eff7d4715ff5bd0f28
Arek <aras250@gmail.com>
2022-11-28 22:31:08 +0100

    Dodaj pliki projektów.

---
+++ Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace iMievCan
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgPids = new System.Windows.Forms.DataGridView();
+            this.cbSerialPorts = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSerialConnect = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.Title = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.pid = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bit_1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bit_2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bit_3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bit_4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bit_5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bit_6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bit_7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bit_8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.formula = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.val = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lastupdate = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgPids)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgPids
+            // 
+            this.dgPids.AllowUserToDeleteRows = false;
+            this.dgPids.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgPids.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgPids.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Title,
+            this.pid,
+            this.bit_1,
+            this.bit_2,
+            this.bit_3,
+            this.bit_4,
+            this.bit_5,
+            this.bit_6,
+            this.bit_7,
+            this.bit_8,
+            this.formula,
+            this.val,
+            this.lastupdate});
+            this.dgPids.Location = new System.Drawing.Point(12, 35);
+            this.dgPids.Name = "dgPids";
+            this.dgPids.Size = new System.Drawing.Size(1209, 612);
+            this.dgPids.TabIndex = 0;
+            this.dgPids.CellBeginEdit += new System.Windows.Forms.DataGridViewCellCancelEventHandler(this.dgPids_CellBeginEdit);
+            this.dgPids.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgPids_CellContentClick);
+            this.dgPids.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgPids_CellEndEdit);
+            this.dgPids.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dgPids_UserAddedRow);
+            // 
+            // cbSerialPorts
+            // 
+            this.cbSerialPorts.FormattingEnabled = true;
+            this.cbSerialPorts.Location = new System.Drawing.Point(52, 6);
+            this.cbSerialPorts.Name = "cbSerialPorts";
+            this.cbSerialPorts.Size = new System.Drawing.Size(121, 21);
+            this.cbSerialPorts.TabIndex = 1;
+            this.cbSerialPorts.DropDown += new System.EventHandler(this.cbSerialPorts_DropDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "serial:";
+            // 
+            // btnSerialConnect
+            // 
+            this.btnSerialConnect.Location = new System.Drawing.Point(179, 6);
+            this.btnSerialConnect.Name = "btnSerialConnect";
+            this.btnSerialConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnSerialConnect.TabIndex = 3;
+            this.btnSerialConnect.Text = "Connect";
+            this.btnSerialConnect.UseVisualStyleBackColor = true;
+            this.btnSerialConnect.Click += new System.EventHandler(this.btnSerialConnect_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(713, 4);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // Title
+            // 
+            this.Title.HeaderText = "title";
+            this.Title.Name = "Title";
+            // 
+            // pid
+            // 
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.pid.DefaultCellStyle = dataGridViewCellStyle4;
+            this.pid.HeaderText = "pid";
+            this.pid.Name = "pid";
+            this.pid.Width = 46;
+            // 
+            // bit_1
+            // 
+            this.bit_1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.bit_1.HeaderText = "bit1";
+            this.bit_1.Name = "bit_1";
+            this.bit_1.Width = 49;
+            // 
+            // bit_2
+            // 
+            this.bit_2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.bit_2.HeaderText = "bit2";
+            this.bit_2.Name = "bit_2";
+            this.bit_2.Width = 49;
+            // 
+            // bit_3
+            // 
+            this.bit_3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.bit_3.HeaderText = "bit3";
+            this.bit_3.Name = "bit_3";
+            this.bit_3.Width = 49;
+            // 
+            // bit_4
+            // 
+            this.bit_4.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.bit_4.HeaderText = "bit4";
+            this.bit_4.Name = "bit_4";
+            this.bit_4.Width = 49;
+            // 
+            // bit_5
+            // 
+            this.bit_5.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.bit_5.HeaderText = "bit5";
+            this.bit_5.Name = "bit_5";
+            this.bit_5.Width = 49;
+            // 
+            // bit_6
+            // 
+            this.bit_6.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.bit_6.HeaderText = "bit6";
+            this.bit_6.Name = "bit_6";
+            this.bit_6.Width = 49;
+            // 
+            // bit_7
+            // 
+            this.bit_7.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.bit_7.HeaderText = "bit7";
+            this.bit_7.Name = "bit_7";
+            this.bit_7.Width = 49;
+            // 
+            // bit_8
+            // 
+            this.bit_8.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.bit_8.HeaderText = "bit8";
+            this.bit_8.Name = "bit_8";
+            this.bit_8.Width = 49;
+            // 
+            // formula
+            // 
+            this.formula.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.formula.DefaultCellStyle = dataGridViewCellStyle5;
+            this.formula.HeaderText = "formula";
+            this.formula.Name = "formula";
+            this.formula.Width = 66;
+            // 
+            // val
+            // 
+            this.val.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle6.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.val.DefaultCellStyle = dataGridViewCellStyle6;
+            this.val.HeaderText = "val";
+            this.val.Name = "val";
+            // 
+            // lastupdate
+            // 
+            this.lastupdate.HeaderText = "lastupdate";
+            this.lastupdate.Name = "lastupdate";
+            this.lastupdate.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1233, 659);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnSerialConnect);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbSerialPorts);
+            this.Controls.Add(this.dgPids);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgPids)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgPids;
+        private System.Windows.Forms.ComboBox cbSerialPorts;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSerialConnect;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Title;
+        private System.Windows.Forms.DataGridViewTextBoxColumn pid;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bit_1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bit_2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bit_3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bit_4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bit_5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bit_6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bit_7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn bit_8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn formula;
+        private System.Windows.Forms.DataGridViewTextBoxColumn val;
+        private System.Windows.Forms.DataGridViewTextBoxColumn lastupdate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace iMievCan
+{
+    public partial class Form1 : Form
+    {
+        System.IO.Ports.SerialPort Serial;
+        class PID
+        {
+            public string _PID;
+
+            public static List<PID> _list = new List<PID>();
+
+            public DataGridViewRow row = null;
+
+            public bool CanCalculate = false;
+            public string[] serialData = { "0", "0", "0", "0", "0", "0", "0", "0" };
+            private PID(string p_PID)
+            {
+                _PID = p_PID;
+            }
+
+            public static PID Add(string p_PID, DataGridViewRow p_Row)
+            {
+                PID p = new PID(p_PID);
+                _list.Add(p);
+                p.row = p_Row;
+                p.row.Tag = p;
+                p.row.Cells["pid"].Value = p_PID;
+                return p;
+            }
+
+            public void NewData(string[] data)
+            {
+                if (row != null)
+                {
+                    row.Cells[2].Value = data[0];
+                    row.Cells[3].Value = data[1];
+                    row.Cells[4].Value = data[2];
+                    row.Cells[5].Value = data[3];
+                    row.Cells[6].Value = data[4];
+                    row.Cells[7].Value = data[5];
+                    row.Cells[8].Value = data[6];
+                    row.Cells[9].Value = data[7];
+                    row.Cells[12].Value = DateTime.Now.ToLongTimeString();
+
+                    serialData = data;
+                    for (int i = 0; i <= serialData.Length - 1; i++)
+                    {
+                        serialData[i] = serialData[i].Replace("\r", "");
+                    }
+                    CalculateVal();
+                }
+            }
+
+            public void CalculateVal()
+            {
+                if (serialData != null && serialData.Length >= 8)
+                {
+                    string formula = (string)row.Cells[10].Value;
+                    if (!string.IsNullOrEmpty(formula) && formula.Trim() != "")
+                    {
+                        formula = formula.Replace("b0", Convert.ToByte(serialData[0], 16).ToString());
+                        formula = formula.Replace("b1", Convert.ToByte(serialData[1], 16).ToString());
+                        formula = formula.Replace("b2", Convert.ToByte(serialData[2], 16).ToString());
+                        formula = formula.Replace("b3", Convert.ToByte(serialData[3], 16).ToString());
+                        formula = formula.Replace("b4", Convert.ToByte(serialData[4], 16).ToString());
+                        formula = formula.Replace("b5", Convert.ToByte(serialData[5], 16).ToString());
+                        formula = formula.Replace("b6", Convert.ToByte(serialData[6], 16).ToString());
+                        formula = formula.Replace("b7", Convert.ToByte(serialData[7], 16).ToString());
+                        row.Cells[11].Value = Evaluate(formula);
+                    }
+                }
+            }
+
+        }
+
+        public Form1()
+        {
+
+
+            InitializeComponent();
+        }
+
+
+
+        public static double Evaluate(string expression)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                table.Columns.Add("expression", typeof(string), expression);
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+                return double.Parse((string)row["expression"]);
+            }
+            catch { return -9999; }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+
+        private void cbSerialPorts_DropDown(object sender, EventArgs e)
+        {
+            cbSerialPorts.Items.Clear();
+            foreach (string PortName in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                cbSerialPorts.Items.Add(PortName);
+            }
+        }
+
+        private void btnSerialConnect_Click(object sender, EventArgs e)
+        {
+
+            CloseSerial();
+
+            Serial = new System.IO.Ports.SerialPort(cbSerialPorts.Text, 9600, System.IO.Ports.Parity.None, 8, System.IO.Ports.StopBits.One);
+            Serial.Handshake = System.IO.Ports.Handshake.None;
+            Serial.DtrEnable = true;
+            Serial.DataReceived += Serial_DataReceived;
+
+            Serial.Open();
+        }
+
+        private void Serial_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+
+            try
+            {
+                String serialString = Serial.ReadLine().Replace("\r", string.Empty);
+
+                string[] serialData = serialString.Split(':');
+                if (serialData.Length == 9)
+                {
+                    if (serialData[0] != "0" && serialData[0].Length == 3)
+                    {
+                        BeginInvoke((Action)(() =>
+                        {
+                            evaluateResponse(serialData);
+                        }));
+                    }
+                }
+            }
+            catch { }
+        }
+
+        void evaluateResponse(string[] serialData)
+        {
+            string senderPID = serialData[0];
+
+            List<PID> pids = new List<PID>();
+
+            for (int i = 0; i < serialData.Length - 1; i++)
+            {
+                serialData[i] = serialData[i];
+            }
+
+            foreach (PID p in PID._list)
+            {
+                if (p._PID == serialData[0]) pids.Add(p);
+            }
+
+            if (pids.Count == 0)
+            {
+                DataGridViewRow newRow = dgPids.Rows[dgPids.Rows.Add()];
+                pids.Add(PID.Add(senderPID, newRow));
+            }
+
+            foreach (PID pid in pids)
+            {
+                pid.NewData(new string[] { serialData[1], serialData[2], serialData[3], serialData[4], serialData[5], serialData[6], serialData[7], serialData[8] });
+            }
+
+        }
+        void CloseSerial()
+        {
+            if (Serial != null)
+            {
+                Serial.DataReceived -= Serial_DataReceived;
+                Serial.DtrEnable = false;
+                Serial.DiscardInBuffer();
+                if (Serial.IsOpen) { Serial.Close(); }
+                Serial = null;
+            }
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseSerial();
+            SaveData();
+        }
+
+        private void dgPids_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 10)
+                {
+                    PID p = (PID)dgPids.Rows[e.RowIndex].Tag;
+                    p.CanCalculate = p.row.Cells[10].Value.ToString().Trim() != "";
+                    p.CalculateVal();
+                }
+                else if (e.ColumnIndex == 1)
+                {
+                    DataGridViewRow newRow = dgPids.Rows[e.RowIndex];
+                    if (newRow.Tag == null)
+                    {
+                        if (newRow.Cells[1].Value != null && newRow.Cells[1].Value.ToString().Trim() != "")
+                        {
+                            string s_newPID = newRow.Cells[1].Value.ToString();
+
+                            string[] data = { "0", "0", "0", "0", "0", "0", "0", "0" };
+                            foreach (PID pid in PID._list)
+                            {
+                                if (pid._PID == s_newPID)
+                                {
+                                    for (int i = 0; i <= pid.serialData.Length - 1; i++)
+                                    {
+                                        data[i] = pid.serialData[i].ToString();
+                                    }
+                                }
+                            }
+
+                            PID.Add(s_newPID, newRow).NewData(data);
+                        }
+                        else
+                        {
+                            dgPids.Rows.Remove(newRow);
+                        }
+                    }
+
+                }
+            }
+            catch { }
+        }
+
+        private void dgPids_CellBeginEdit(object sender, DataGridViewCellCancelEventArgs e)
+        {
+            try
+            {
+                if (e.ColumnIndex == 10)
+                {
+                    PID p = (PID)dgPids.Rows[e.RowIndex].Tag;
+                    p.CanCalculate = false;
+
+                }
+            }
+            catch { }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+        void SaveData()
+        {
+            string sToSave = "";
+            foreach (DataGridViewRow row in dgPids.Rows)
+            {
+                string sRow = "";
+                if (row.Tag == null) continue;
+
+                for (int i = 0; i <= row.Cells.Count - 1; i++)
+                {
+                    string sVal = "";
+                    if (row.Cells[i].Value != null) sVal = row.Cells[i].Value.ToString();
+                    sRow += sVal + ";";
+                }
+                sToSave += sRow + "\n";
+
+            }
+            System.IO.File.WriteAllText(System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\data.csv", sToSave);
+        }
+        void LoadData()
+        {
+            string sFilePath = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"\data.csv";
+            if (System.IO.File.Exists(sFilePath))
+            {
+                string[] lines = System.IO.File.ReadAllLines(sFilePath);
+
+                foreach (string line in lines)
+                {
+                    string[] data = line.Split(';');
+                    if (data.Length >= 12)
+                    {
+                        string senderPID = data[1];
+                        List<string> lData = new List<string>();
+                        lData.AddRange(data);
+                        while (lData.Count < dgPids.Columns.Count)
+                        {
+                            lData.Add("");
+                        }
+                        data = lData.ToArray();
+                        DataGridViewRow newRow = dgPids.Rows[dgPids.Rows.Add(data)];
+                        PID pid = PID.Add(senderPID, newRow);
+
+                        pid.NewData(new string[] { data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9] });
+
+                        newRow.Cells["lastupdate"].Value = data[12];
+                    }
+                }
+            }
+        }
+
+        private void dgPids_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {
+            try
+            {
+
+
+
+            }
+            catch { }
+
+        }
+
+        private void dgPids_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
